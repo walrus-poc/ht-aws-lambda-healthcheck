27b0d6bbd0a6cd4526a8cddf52bec656c748dc19
pengjiang80 <peng.jiang@hotmail.com>
2024-03-03 13:21:04 +0800

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
